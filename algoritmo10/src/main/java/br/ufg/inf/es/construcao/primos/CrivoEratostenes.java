8479c4a3ada02fa74ff682442c885135b64e33c7
matheuspiment <matheuspiment@hotmail.com>
2015-11-05 20:28:54 -0200

    Update algoritmo10

---
+++ algoritmo10/src/main/java/br/ufg/inf/es/construcao/primos/CrivoEratostenes.java
@@ -0,0 +1,52 @@
+package br.ufg.inf.es.construcao.primos;
+
+/**
+ * Implementação do algoritmo que simula o crivo de Erastóstenes.
+ */
+public class CrivoEratostenes {
+
+    /**
+     * Simula o crivo de Erastóstenes. Caso o número seja ímpar
+     * sua posição no vetor assume valor 0, caso contrário, assume valor 1.
+     *
+     * @param a Valores de entrada do vetor.
+     * @param n Número cuja primaridade será verificada.
+     * @throws IllegalArgumentException Caso o n, o tamanho do vetor ou os
+     * valores posteriores a segunda posição do vetor fornecidos não atendem ao
+     * valor de "partida" estabelecido.
+     * N deve ser um valor maior que 1.
+     * O tamanho do vetor deve ser maior que n + 1.
+     * Todas as posições do vetor, a partir da segunda, devem ter valor 0.
+     */
+    public static void crivoEratostenes(int[] a, int n) {
+        if (n < 2) {
+            throw new IllegalArgumentException("n inválido");
+        }
+
+        if (a.length < (n + 1)) {
+            throw new IllegalArgumentException("tamanho do vetor");
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (a[i] != 0) {
+                throw new IllegalArgumentException("valor da posição diferente de 0");
+            }
+        }
+
+        int i = 2;
+        int c = 0;
+
+        while (i <= n / 2) {
+            if (a[i] == 0) {
+                c = i + i;
+                while (c <= n) {
+                    a[c] = 1;
+                    c = c + i;
+                }
+            }
+
+            i = i + 1;
+        }
+
+    }
+}
---
+++ algoritmo10/src/test/java/br/ufg/inf/es/construcao/primos/CrivoEratostenesTest.java
@@ -0,0 +1,51 @@
+package br.ufg.inf.es.construcao.primos;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class CrivoEratostenesTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNInválido() {
+        CrivoEratostenes.crivoEratostenes(new int[] {1, 2}, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testTamanhoInvalidoDoVetor() {
+        CrivoEratostenes.crivoEratostenes(new int[] {1, 2, 0, 0}, 4);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testValoresInvalidosDoVetor() {
+        CrivoEratostenes.crivoEratostenes(new int[] {1, 2, 3, 0}, 2);
+    }
+
+    @Test
+    public void testCincoPrimo() {
+        int[] a = new int[6];
+        CrivoEratostenes.crivoEratostenes(a, 5);
+        Assert.assertEquals(0, a[5]);
+    }
+
+    @Test
+    public void testSetePrimo() {
+        int[] a = {1, 2, 0, 0, 0, 0, 0, 0};
+        CrivoEratostenes.crivoEratostenes(a, 7);
+        Assert.assertEquals(0, a[7]);
+    }
+
+    @Test
+    public void testQuatroNaoPrimo() {
+        int[] a = {0, 0, 0, 0, 0};
+        CrivoEratostenes.crivoEratostenes(a, 4);
+        Assert.assertEquals(1, a[4]);
+    }
+
+    @Test
+    public void testCinquentaNaoPrimo() {
+        int[] a = new int[51];
+        CrivoEratostenes.crivoEratostenes(a, 50);
+        Assert.assertEquals(1, a[50]);
+    }
+
+}
