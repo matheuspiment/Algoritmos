d1d249d6879c6cf72d15c17e43676ef57083f374
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2015-11-08 14:40:17 -0200

    Update Log.java

--- algoritmo18/src/main/java/br/ufg/inf/es/construcao/matematica/Log.java
+++ algoritmo18/src/main/java/br/ufg/inf/es/construcao/matematica/Log.java
@@ -1,32 +1,32 @@
 package br.ufg.inf.es.construcao.matematica;
 
 /**
- * ImplementaÁ„o do algoritmo que calcula o log natural com base na soma
- * dos termos 1 + n + (n * 2) / 2! + ... + (n ^ (k - 1) / (k - 1)!) +
+ * Implementa√ß√£o do algoritmo que calcula o log natural com base na soma
+ * dos termos 1 + n + (n ^ 2) / 2! + ... + (n ^ (k - 1) / (k - 1)!) +
  * (n ^ k / k!).
  */
 public class Log {
 
     /**
-     * Calcula o log natural com base na somatÛria de termos.
+     * Calcula o log natural com base na somat√≥ria de termos.
      *
-     * @param n N˙mero ao qual se pretende obter o log.
-     * @param k Define a quantidade de termos a serem somados(precis„o).
+     * @param n N√∫mero ao qual se pretende obter o log.
+     * @param k Define a quantidade de termos a serem somados(precis√£o).
      *
      * @return O log.
      *
      * @throws IllegalArgumentException Caso n ou k estejam abaixo do limite
      * de entrada estabelecido.
-     * O par‚metro n deve ser maior que 0.
-     * O par‚metro k deve ser maior que 1.
+     * O par√¢metro n deve ser maior que 0.
+     * O par√¢metro k deve ser maior que 1.
      */
     public static double log(int n, int k) {
         if (n < 1) {
-            throw new IllegalArgumentException("n inv·lido");
+            throw new IllegalArgumentException("n inv√°lido");
         }
 
         if (k < 2) {
-            throw new IllegalArgumentException("k inv·lido");
+            throw new IllegalArgumentException("k inv√°lido");
         }
 
         int i = 2;
