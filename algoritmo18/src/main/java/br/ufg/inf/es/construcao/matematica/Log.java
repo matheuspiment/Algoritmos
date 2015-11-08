39136b07773c1521d6c4118d2a181f8b3289d1de
matheuspiment <matheuspiment@hotmail.com>
2015-11-08 14:11:50 -0200

    Add documentação

--- algoritmo18/src/main/java/br/ufg/inf/es/construcao/matematica/Log.java
+++ algoritmo18/src/main/java/br/ufg/inf/es/construcao/matematica/Log.java
@@ -1,6 +1,25 @@
 package br.ufg.inf.es.construcao.matematica;
 
+/**
+ * Implementação do algoritmo que calcula o log natural com base na soma
+ * dos termos 1 + n + (n * 2) / 2! + ... + (n ^ (k - 1) / (k - 1)!) +
+ * (n ^ k / k!).
+ */
 public class Log {
+
+    /**
+     * Calcula o log natural com base na somatória de termos.
+     *
+     * @param n Número ao qual se pretende obter o log.
+     * @param k Define a quantidade de termos a serem somados(precisão).
+     *
+     * @return O log.
+     *
+     * @throws IllegalArgumentException Caso n ou k estejam abaixo do limite
+     * de entrada estabelecido.
+     * O parâmetro n deve ser maior que 0.
+     * O parâmetro k deve ser maior que 1.
+     */
     public static double log(int n, int k) {
         if (n < 1) {
             throw new IllegalArgumentException("n inválido");
