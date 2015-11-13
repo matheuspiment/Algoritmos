c1e511f642c457d3f11f32f1ac94c0ae747cdef5
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2015-11-13 09:43:59 -0200

    melhora na documentação

--- algoritmo28/src/main/java/br/ufg/inf/es/construcao/matematica/Divide.java
+++ algoritmo28/src/main/java/br/ufg/inf/es/construcao/matematica/Divide.java
@@ -5,29 +5,29 @@ import br.ufg.inf.es.construcao.matematica.Mod;
 import br.ufg.inf.es.construcao.matematica.Produto;
 
 /**
- *
+ * Implementação do algoritmo que calcula o resultado de uma divisão
  */
 public class Divide {
 
     /**
-     *
+     * Calcula a divisão entre dois dados números.
      *
      * @param a Dividendo.
      * @param b Divisor.
-     * @param k
+     * @param k Indica a precisão(casas decimais).
      *
      * @throws IllegalArgumentException Caso os valores de a ou b sejam menores
      * que os valores de entrada estabelecidos.
-     * O par�metro a deve ser maior que 0.
-     * O par�metro b deve ser maior que 0.
+     * O parâmetro a deve ser maior que 0.
+     * O parâmetro b deve ser maior que 0.
      */
     public static void divide(int a, int b, int k) {
         if (a <= 0) {
-            throw new IllegalArgumentException("a inv�lido");
+            throw new IllegalArgumentException("a inválido");
         }
 
         if (b <= 0) {
-            throw new IllegalArgumentException("b inv�lido");
+            throw new IllegalArgumentException("b inválido");
         }
 
         int d = DivideSomas.divideSomas(a, b);
